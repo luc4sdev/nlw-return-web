b9d6afd6c17cab0e9f5d85fba08bfe7eadec21e3
Lucas Pereira <lucasgamesdark@gmail.com>
2022-05-15 11:40:30 -0300

    Projeto Final

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Widget } from "./components/Widget"
+
+function Button() {
+  return <button className="bg-indigo-500 p-2">Hello</button>
+}
+
+export function App() {
+  return (
+    <>
+      <Widget />
+    </>
+  )
+}
+
